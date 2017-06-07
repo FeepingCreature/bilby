09b7b96a068059f094d927c9139b49e4b34e8724
FeepingCreature <default_357-line@yahoo.de>
2017-06-08 00:47:09 +0200

    Allow running arbitrary scripts

---
+++ src/main.c
@@ -0,0 +1,33 @@
+#include "bilby.h"
+
+#include <rdparse/parser.h>
+#include <stdio.h>
+
+int main(int argc, const char **argv) {
+  set_user_clean_fn(eat_bilby_filler);
+  
+  if (argc == 1) {
+    fprintf(stderr, "Usage: %s <file.bb>\n", argv[0]);
+    return 1;
+  }
+  
+  for (int i = 1; i < argc; i++) {
+    const char *filename = argv[i];
+    TextRange filerange = readfile((char*) filename);
+    register_file(filerange, filename, 0, 0);
+    
+    Definitions *defs = alloc_definition_set();
+    setup_runtime(defs);
+    
+    char *source = filerange.start;
+    while (true) {
+      if (parse_tl(&source, defs)) continue;
+      if (eat_string(&source, "\n")) continue;
+      if (source == filerange.end) break;
+      log_parser_error(source, "Unexpected input.");
+      abort();
+    }
+  }
+  return 0;
+}
+
